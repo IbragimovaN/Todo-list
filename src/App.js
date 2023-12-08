04aee3d6b20129ebdfcadf7886836e763252b33c
Anastasiya <anastasiya.ibr@mail.ru>
2023-12-08 22:37:05 +0300

    первая часть - готово

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import styles from "./app.module.css";
+import { useEffect, useState } from "react";
 
 export const App = () => {
-	return <div className={styles.app}>hello!</div>;
+	const [todos, setTodos] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		fetch("https://jsonplaceholder.typicode.com/todos")
+			.then((data) => data.json())
+			.then((data) => {
+				setTodos(data);
+			})
+			.finally(() => setLoading(false));
+	}, []);
+	return (
+		<div className={styles.app}>
+			<ol className={styles.dataContainer}>
+				{todos.map(({ userId, title }) => {
+					return loading ? (
+						<div className={styles.loader}></div>
+					) : (
+						<div>
+							<li className={styles.todoInner} key={userId}>
+								{title}
+							</li>
+						</div>
+					);
+				})}
+			</ol>
+		</div>
+	);
 };
